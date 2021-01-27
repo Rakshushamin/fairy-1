b6d459c9129f94c8317fe37798fd60945b777ce5
Rakshushamin <75742018+Rakshushamin@users.noreply.github.com>
2021-01-27 12:01:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,68 @@
-const Engine=Matter.Engine;
-const World=Matter.World;
-const Bodies=Matter.Bodies;
-var engine,world;
-var obj;
-var cir;
-function setup() {
-  var canvas=createCanvas(400,400);
-  engine=Engine.create();
-  world=engine.world;
-  var obj_options={
-    isStatic:true
-  }
+var fairy,fairy_img;
+var starngt,starngt_img;
+var star,star_img;
 
-  obj=Bodies.rectangle(200,390,400,50,obj_options);
-  World.add(world,obj);
-  var cir_options={
-    restitution:1
-  }
-  cir=Bodies.circle(200,200,20,cir_options);
-  World.add(world,cir);
-  //console.log(obj.type);
-  //console.log(obj.position);
-  console.log(cir);
+function preload()
+{
+   //preload the images here
+   fairy_img=loadAnimation("fairy1.png","fairy2.png");
+   starngt_img=loadImage("starnight.png");
+   star_img=loadImage("star.png");
 }
 
+function setup() {
+  createCanvas(600,600);
+  fairy=createSprite("150,150,10,10");
+  fairy.addAnimation("Fairy",fairy_img);
+  fairy.scale=0.2;
+
+  starngt=createSprite("300,300,10,10");
+  starngt.addImage("Star",starngt_img);
+  starngt.scale=1;
+
+  star=createSprite("500,500,10,10,star_option1,star_option2");
+  star.addImage("Star",star_img);
+  star.scale=0.5;
+
+  star=createSprite("250,250,10,10,star_option1,star_option2");
+  star.addImage("Star",star_img);
+  star.scale=0.5;
+
+  star=createSprite("450,450,10,10,star_option1,star_option2");
+  star.addImage("Star",star_img);
+  star.scale=0.37;
+
+  star=createSprite("300,300,10,10,star_option1,star_option2");
+  star.addImage("Star",star_img);
+  star.scale=0.2;
+}
+
+
 function draw() {
-  background("green");  
-  Engine.update(engine);
-  rectMode(CENTER);
-  rect(obj.position.x,obj.position.y,400,50);
-  ellipseMode(RADIUS);
-  ellipse(cir.position.x,cir.position.y,20);
+  background("black");
+  if(keyDown("right"))
+    {
+      fairy.x=fairy.x+3;
+    }
+  if(keyDown("left"))
+    {
+      fairy.x=fairy.x-3;
+    }
+  if(keyDown("space"))
+  {
+    var star_option1=
+    {
+    
+      isStatic:false
+    }
+  }
+  if(star.isTouching("fairy"))
+  {
+    var star_option2=
+    {
+    
+      isStatic:true
+    }
+  }
+  drawSprites();
 }
